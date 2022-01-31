305073a84075915cad510b188cfc3fc8b4e1c708
tan-xianglong <t.xianglong@gmail.com>
2022-01-31 23:37:48 +0800

    first commit navbar mobile created

---
+++ script.js
@@ -0,0 +1,9 @@
+// function to toggle mobile navbar
+const navToggle = () => {
+  const x = document.querySelector('#link-container-mobile');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
